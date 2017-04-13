0f37b0a2f63c79fc1e96299383536a59aab38fae
anugonda <sravanianugonda@gmail.com>
2017-04-13 17:30:59 -0400

    Add files via upload

---
+++ GetFileMetaData.java
@@ -0,0 +1,18 @@
+
+@Component
+public class GetFileMetaData{
+
+
+	@Value("${nls.search.serviceName}")
+	private String serviceName;
+	
+	@Value("${nls.search.roleName}")
+	private String roleName;
+	
+	@Value("${nls.search.userName}")
+	private String userName;
+	
+
+	
+	
+}
